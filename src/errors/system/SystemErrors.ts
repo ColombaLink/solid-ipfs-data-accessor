414f1de959cced02f534d2e4a30bfb8ac47334b7
fuubi <fuubi@protonmail.ch>
2021-08-23 09:36:33 +0200

    Chore: Add comments to system errors.

--- src/errors/system/SystemErrors.ts
+++ src/errors/system/SystemErrors.ts
@@ -5,8 +5,13 @@ import type { SystemError } from '@solid/community-server';
 // https://filippo.io/linux-syscall-table/
 
 /**
+ * Invalid argument. An invalid value was given for one of the arguments to a function.
+ * For example, the value given for the origin when positioning a file pointer
+ * (by means of a call to fseek) is before the beginning of the file.
+ * [DocSource](https://docs.microsoft.com/en-us/cpp/c-runtime-library/errno-constants?view=msvc-160)
  *
- * @constructor
+ * @param error
+ * @param syscall
  */
 export function systemErrorInvalidArgument(error: Error, syscall: string): SystemError {
   const systemError: SystemError = error as SystemError;
@@ -16,11 +21,17 @@ export function systemErrorInvalidArgument(error: Error, syscall: string): Syste
   return systemError;
 }
 
+/**
+ * Directory not empty.
+ * @param error
+ * @param syscall
+ * @param path
+ */
 export function systemErrorNotEmptyDir(error: Error, syscall: string, path: string): SystemError {
   const systemError: SystemError = error as SystemError;
   systemError.code = 'ENOTEMPTY';
   systemError.syscall = syscall;
-  systemError.errno = -39;
+  systemError.errno = 39;
   systemError.path = path;
   return systemError;
 }
@@ -29,8 +40,8 @@ export function systemErrorNotEmptyDir(error: Error, syscall: string, path: stri
  * No such file or directory.
  * The specified file or directory does not exist or cannot be found.
  * This message can occur whenever a specified file does not exist or a component of a path does not specify an existing directory.
- *
  * [DocSource](https://docs.microsoft.com/en-us/cpp/c-runtime-library/errno-constants?view=msvc-160)
+ *
  * @param error
  * @param syscall
  * @param path
